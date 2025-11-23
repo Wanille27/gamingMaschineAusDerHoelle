6982e239f9cf36600991a5e36f60b9eac9881f6d
JonasDe17 <Jonas.Jabs@araymond.com>
2025-11-23 18:36:12 +0100

    BitKnight

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -3,13 +3,11 @@ extends Area2D
 @onready var timer = $Timer
 
 func _on_body_entered(body):
-	if not MultiplayerManager.multiplayer_mode_enabled:
+	#if not MultiplayerManager.multiplayer_mode_enabled:
 		print("You died!")
 		Engine.time_scale = 0.5
 		body.get_node("CollisionShape2D").queue_free()
 		timer.start()
-	else:
-		_multiplayer_dead(body)
 
 func _multiplayer_dead(body):
 	if multiplayer.is_server() && body.alive:
--- startscreen.gd
+++ startscreen.gd
@@ -1,4 +1,5 @@
 extends Node2D
 
-func _on_start_button_down() -> void:
-	get_tree().change_scene_to_file("res://scenes/game.tscn")
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("start_game"):
+		get_tree().change_scene_to_file("res://scenes/game.tscn")
